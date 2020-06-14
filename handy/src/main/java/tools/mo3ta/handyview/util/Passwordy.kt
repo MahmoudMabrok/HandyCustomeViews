cc6dd97533beff2197bb415a775bfd73028757a8
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2020-06-14 22:19:04 +0200

    start with passwordy

---
+++ handy/src/main/java/tools/mo3ta/handyview/util/Passwordy.kt
@@ -0,0 +1,17 @@
+package tools.mo3ta.handyview.util
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class Passwordy @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+
+
+    init {
+
+
+    }
+}
