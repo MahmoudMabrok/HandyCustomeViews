6969f87e0d09635a5a8ee542eb1032367d3e5972
Mahmoud Mabrok Fouad <mahmoudmabrok3579@gmail.com>
2020-06-15 22:19:06 +0200

    add passwrod layout & add change visibiltiy of passwords

--- handy/src/main/java/tools/mo3ta/handyview/util/Passwordy.kt
+++ handy/src/main/java/tools/mo3ta/handyview/util/Passwordy.kt
@@ -1,17 +1,41 @@
 package tools.mo3ta.handyview.util
 
 import android.content.Context
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.EditText
 import androidx.constraintlayout.widget.ConstraintLayout
+import kotlinx.android.synthetic.main.password.view.*
+import tools.mo3ta.handyview.R
 
 class Passwordy @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-
+    private var isHidden = true
 
     init {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.password, this)
 
 
+        toggle.setOnClickListener {
+            if (isHidden) {
+                // so show
+                edPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+            } else {
+                // hide
+                edPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            }
+            // change state
+            isHidden = isHidden.not()
+        }
     }
+
+    fun getEdit(): EditText? {
+        return edPassword
+    }
+
 }
